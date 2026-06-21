d4daf32d519b0e9f6c9f1d9278be406b7e8a142c
Naonimes <137807925+Naonimes@users.noreply.github.com>
2026-06-21 20:13:22 +0700

    Add project files.

---
+++ TugasBesarKPLUInterface/DiscountUI.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TugasBesarKPL_UInterface
+{
+    internal class DiscountUI
+    {
+    }
+}
---
+++ TugasBesarKPLUInterface/Form1.cs
@@ -0,0 +1,10 @@
+namespace TugasBesarKPLUInterface
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
