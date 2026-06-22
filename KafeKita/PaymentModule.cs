27c3a46ec39c227f269fde599260fd7c4eb5b137
Khaydir Murteza <khaydirmurteza@gmail.com>
2026-06-22 16:03:07 +0700

    Implementasi paymentModule dan paymentUI

--- KafeKita/PaymentModule.cs
+++ KafeKita/PaymentModule.cs
@@ -1,10 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace TugasBesarKPL_Solution
 {
-    internal class PaymentModule
+    public class PaymentModule
     {
+        public string GenerateReceiptID()
+        {
+            string rawStr = Guid.NewGuid().ToString();
+            byte[] bytes = SHA256.HashData(Encoding.UTF8.GetBytes(rawStr));
+            return Convert.ToHexString(bytes).Substring(0, 8);
+        }
     }
 }
